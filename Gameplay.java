345175c74b00b56fa984d6cbce17a0aa021093c6
Feguette <liang.j4son@gmail.com>
2017-01-24 10:21:00 -0800

    Update Gameplay.java

--- Gameplay.java
+++ Gameplay.java
@@ -13,43 +13,86 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.awt.image.BufferedImage;
 import java.util.*;
-public class Gameplay //extend JPanel
+public class Gameplay //extends JPanel
 {
     public static void main (String args[])
     {
         Deck d = new Deck();
         Player a = new Player();
+        
+        boolean opCont = true;
         a.addCard(d.draw());
         a.addCard(d.draw());
         a.display(d.getDeck());
         a.display(a.getHand());
         int choice;
         Scanner in = new Scanner(System.in);
-        while (opCont)
+        gameLoop:while (opCont)
         {
             playerLoop:while (p1)
             {
                 System.out.println("1. Stay");
                 System.out.println("2. Hit");
-                System.out.println("3. Double Down");
+                //System.out.println("3. Double Down");
                 System.out.println("4. Split");
-                choice = 0;
-                if (choice != {1,2,3,4})
-                    choice = in.nextInt();
+                choice = -1;
+                choice = in.nextInt();
+                if (choice == 0)
+                {
+                    break gameLoop;
+                }
                 
                 if (choice == 1)
                 {
-                    break
+                    break playerLoop;
+                }
+                
+                if (choice == 2)
+                {
+                    a.addCard(d.draw);
+                    p1 = !a.checkLoss();
+                }
+                
+                if (choice == 3)
+                {
+                    a.addCard(d.draw);
+                    break playerLoop;
+                }
+                
+                if (choice == 4)
+                {
+                    
                 }
             }
             
             dealerLoop:while (d1)
             {
-                if (a.checkLoss())
+                if ($player.checkLoss())
                 {
-                    break;
+                    System.out.println("You Lose");
+                    break dealerLoop;
+                }
+                else
+                {
+                    while ($dealer.getTotal() < 17)
+                    {
+                        $dealer.addCard($deck.draw);
+                    }
+                    if ($dealer.getLoss() == true)
+                    {
+                        System.out.println("You win");
+                        break dealer;
+                    }
+                    else
+                    {
+                        if ($dealer.getTotal() < $player.getTotal())
+                        {
+                            
+                        }
+                    }
                 }
             }
         }
     }
-}
+}   
+    
