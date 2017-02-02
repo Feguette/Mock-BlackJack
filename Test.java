85c212746ddc89c4d70034f554271950a1a7615e
Feguette <liang.j4son@gmail.com>
2017-02-01 20:22:49 -0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -34,13 +34,46 @@ public class Test extends JPanel
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         thing.repaint();
+        display(thing.getNoob().getPortion(0));
+        JButton clickEnd = new JButton("End me.");
+        frame.add(clickEnd);
+        class PleaseEnd implements ActionListener
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                System.out.println("This process needs to break.");
+            }
+        }
+        ActionListener pleaseEnd = new PleaseEnd();
+        clickEnd.addActionListener(pleaseEnd);
+        System.out.println("You did it!");
+    }
+    
+    public Player getNoob()
+    {
+        return noob;
+    }
+    
+    public static void display(Hand pack)
+    {
+        String[] suits = {"S", "C", "D", "H"};
+        ArrayList<Card> pile = pack.getHand();
+        for (int i = 0; i < pile.size(); i++)
+        {
+            Card current = pile.get(i);
+            System.out.print(current.getType());
+            System.out.print(suits[current.getSuit()]);
+            if (pile.size() > i + 1)
+                System.out.print(", ");
+        }
+        System.out.println(" = " + pack.getTotal());
     }
     
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
         //g2.drawRect(10, 10, 200, 200); 
-        g2.drawImage(noob.getPortion(0).getCard(0).getImage(), 0, 0, null);
-        g2.drawImage(noob.getPortion(0).getCard(1).getImage(), 25, 0 , null);
+        g2.drawImage(noob.getPortion(0).getCard(0).getImage(), 300, 300, null);
+        g2.drawImage(noob.getPortion(0).getCard(1).getImage(), 325, 300 , null);
     }
 }
